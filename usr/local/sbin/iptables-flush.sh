e017c9bacf5c5216ac29fa32ab0ffaa156a7597b
WEPN <support@we-pn.com>
2025-02-15 03:58:07 -0800

    fix missed table for wireguard rule

--- usr/local/sbin/iptables-flush.sh
+++ usr/local/sbin/iptables-flush.sh
@@ -3,4 +3,4 @@ iptables -t nat -F
 ip6tables -t nat -F
 iptables -A OUTPUT -p icmp -j REJECT
 # needed for wireguard
-iptables -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
